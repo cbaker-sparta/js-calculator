d5d592848d0cacab36c0c26b496c0f95a18daf79
Tech-A24 <tech-a24@Tech-A24s-MacBook-Air.local>
2019-04-09 09:47:32 +0100

    updated

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Dual calculator function for working out basic sums as well as working out the square root and power of give numbers.
 
-var calculatorFunction = prompt("Which calculator would you like to use: (b)asic or (a)dvanced)?");
+var calculatorFunction = prompt("Which calculator would you like to use: (1)basic, (2)advanced), (3)BMI?");
 
-if (calculatorFunction.toLowerCase() == "b") {
+if (calculatorFunction == "1") {
   var stringBasicNum1 = prompt("Please enter first number:", "0");
   var basicOperator = prompt("Please enter the operator (+ , - , / , *):");
   var stringBasicNum2 = prompt("Please enter second number:", "0");
@@ -24,7 +24,7 @@ if (calculatorFunction.toLowerCase() == "b") {
       alert("Invalid entry. Please try again.")
     }
 }
-else if (calculatorFunction.toLowerCase() == "a") {
+else if (calculatorFunction == "2") {
   var stringAdvNum1 = prompt("Please enter the number:", "0");
   var advOperator = prompt("Please choose square root(1) or power(2):");
   var aNum1 = parseInt(stringAdvNum1);
@@ -40,9 +40,19 @@ else if (calculatorFunction.toLowerCase() == "a") {
       alert("Invalid entry. Please try again.")
     }
 }
+else if (calculatorFunction == "3") {
+  var stringBmiNum1 = prompt("Please enter your height:", "0");
+  var stringBmiNum2 = prompt("Please enter your weight:", "0");
+  var bmiNum1 = parseInt(stringBmiNum1);
+  var bmiNum2 = parseInt(stringBmiNum2);
+    if (bmiNum1 != null && bmiNum2 != null) {
+      alert("Your BMI is: " + (bmiNum2 / Math.sqrt(bmiNum1)));
+    }
+    else {
+      alert("Invalid entry. Please try again.")
+    }
+}
 else {
   alert("Invalid entry. Please try again.")
   var calculatorFunction = prompt("Which calculator would you like to use? ((b)asic or (a)dvanced)");
 }
-
-/
